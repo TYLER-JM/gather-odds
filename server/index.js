8cefe88bc702733c73d919928e6cde0ecab3b893
tyler martin <t.mar92@gmail.com>
2020-01-10 16:54:40 -0500

    moves all routes to index, no longer uses oddsRoutes

--- server/index.js
+++ server/index.js
@@ -4,24 +4,23 @@ const PORT = 8080;
 const express =  require('express');
 const app = express();
 
-const api = require('../lib/api.js');
+const api = require('../lib/api');
 
 app.use(express.static('public'));
 
-const oddsRoutes = require('./oddsRoutes.js')();
-app.use('/odds', oddsRoutes);
+// const oddsRoutes = require('./oddsRoutes.js')();
+// app.use('/odds', oddsRoutes);
 
-// app.get('/odds', (req, res) => {
-//   // let result = api(req);
-//   console.log('the req: ', req);
-//   // console.log(result);
-//   res.status(400).end();
-//   // res.writeHead(200, {'Content-Type': 'application/json' });
-//   // res.write(JSON.stringify({status: OK }));
-//   // res.status(400);
-//   // res.send("hey");
-// });
-
+app.get('/odds', async (req, res) => {
+  // console.log('the req: ', req);
+  try {
+    let result = await api();
+    console.log("after the api: ", result);
+    res.send(result);
+  } catch (err) {
+    console.log("What's wrong???", err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- server/oddsRoutes.js
+++ server/oddsRoutes.js
@@ -5,12 +5,12 @@ module.exports = () => {
   router.get('/odds', (req, res) => {
     // let result = api(req);
     console.log('the req: ', req);
-    console.log(result);
-    res.status(400).end();
+    // console.log(result);
+    // res.status(400).end();
     // res.writeHead(200, {'Content-Type': 'application/json' });
     // res.write(JSON.stringify({status: OK }));
     // res.status(400);
-    // res.send("hey");
+    res.send("hey");
   });
 
   return router;
