396e73bb0bf7272006ecdfe258f4b4419e278fc6
tyler martin <t.mar92@gmail.com>
2020-01-12 11:08:33 -0500

    adds league selector, and uses the variable in the url

--- public/scripts/getOdds.js
+++ public/scripts/getOdds.js
@@ -1,14 +1,19 @@
-const makeCall = (league) => {
+const makeCall = () => {
+  // const leagues = {
+  //   nfl: 2,
+  //   nba: 4,
+  //   nhl: 6
+  // };
   axios({
     "method":"GET",
-    "url":"https://therundown-therundown-v1.p.rapidapi.com/sports/6/events/2020-01-11",
+    "url":"https://therundown-therundown-v1.p.rapidapi.com/sports/6/events/2020-01-12",
     "headers":{
     "content-type":"application/octet-stream",
     "x-rapidapi-host":"therundown-therundown-v1.p.rapidapi.com",
     "x-rapidapi-key":"6a75e4cf8fmsh4aa85bcc9940ceap11141ajsn3fea21c90b0c"
     },"params":{
     "include":["all_periods",
-    "scores"],"offset":"0"
+    "scores"],"offset":"300"
     }
     })
     .then((response)=>{
@@ -31,7 +36,11 @@ const makeCall = (league) => {
 
 $(function() {
   $('#getOddsBtn').on('click', () => {
+    // e.preventDefault();
+    // let league = $('#league').val();
+    // console.log(league);
     console.log('button pushed');
+    // makeCall($('#league').val());
     makeCall();
     // const res = await $.ajax('/odds', {method: 'GET'});
   });
