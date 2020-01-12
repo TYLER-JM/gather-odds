98f2ca263cc95cb2e225f5fef60fe98b4d52a479
tyler martin <t.mar92@gmail.com>
2020-01-12 09:51:18 -0500

    adds axios to client side with CDN, and changes function call accordingly

--- public/scripts/getOdds.js
+++ public/scripts/getOdds.js
@@ -1,11 +1,31 @@
-const makeCall = async () => {
-  try {
-    //res will be the return value of the api function I write
-    const res = await $.ajax('/odds', {method: 'GET'});
-    console.log("the result of the call: ", res);
-  } catch (err) {
-    console.error(err);
-  }
+const makeCall = (league) => {
+  axios({
+    "method":"GET",
+    "url":"https://therundown-therundown-v1.p.rapidapi.com/sports/6/events/2020-01-11",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"therundown-therundown-v1.p.rapidapi.com",
+    "x-rapidapi-key":"6a75e4cf8fmsh4aa85bcc9940ceap11141ajsn3fea21c90b0c"
+    },"params":{
+    "include":["all_periods",
+    "scores"],"offset":"0"
+    }
+    })
+    .then((response)=>{
+      console.log("response after successful call: ", response)
+      return response;
+      // rez = response;
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  // try {
+  //   //res will be the return value of the api function I write
+  //   const res = await $.ajax('/odds', {method: 'GET'});
+  //   console.log("the result of the call: ", res);
+  // } catch (err) {
+  //   console.error(err);
+  // }
   
 };
 
