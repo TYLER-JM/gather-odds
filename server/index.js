f212d15157991338ea8cd691ab9f9a4d17a09b28
tyler martin <t.mar92@gmail.com>
2020-01-13 16:24:13 -0500

    changes related to trying to get template engine working

--- server/index.js
+++ server/index.js
@@ -12,14 +12,17 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('index');
 })
-app.get('/odds/:league', async (req, res) => {
-  try {
-    let result = await api(req.params.league);
-    // console.log("after the api: ", result);
-    res.send(JSON.stringify(result.data));
-  } catch (err) {
-    console.log("What's wrong???", err);
-  }
+// app.get('/odds/:league', async (req, res) => {
+//   try {
+//     let result = await api(req.params.league);
+//     // console.log("after the api: ", result);
+//     res.send(JSON.stringify(result.data));
+//   } catch (err) {
+//     console.log("What's wrong???", err);
+//   }
+// });
+app.get('/odds/:league', (req, res) => {
+  res.render('table.ejs');
 });
 
 app.listen(PORT, () => {
