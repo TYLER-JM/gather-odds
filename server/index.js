55ccd9dc609d39a09e0a8d1391220a14b11f991e
Tyler Martin <tyler.martin@electric-playground.com>
2020-09-11 19:31:32 -0300

    updates odds route to handle missing books

--- server/index.js
+++ server/index.js
@@ -25,21 +25,47 @@ app.get('/test', (req, res) => {
 });
 
 app.post('/odds', async (req, res) => {
-  // let result = await api(req.body.league);
-  // let books = await api();
-  // let sortedBooks = {};
-  // books.data.affiliates.forEach(book => {
-  //   sortedBooks[book.affiliate_id] = book.affiliate_name;
-  // });
+  let result = await api(req.body.league);
+  let books = await api();
+
+  let sortedBooks = {};
+  books.data.affiliates.forEach(book => {
+    sortedBooks[book.affiliate_id] = book.affiliate_name;
+  });
+
+
+  let myEvents = [];
+  result.data.events.forEach(event => {
+  // sampleData.events.forEach(event => {
+    let lines = [];
+    for (let id in sortedBooks) {
+    // for (let id in sampleData.sortedBooks) {
+      lines.push(event.lines[id] || {});
+    }
+    myEvents.push({
+      'teams': event.teams,
+      'lines': lines,
+    });
+  });
+
+  
+
   // let templateVars = {
   //   events: result.data.events,
   //   // books: books.data.affiliates,
   //   sortedBooks: sortedBooks
   // }
 
-  let templateVars = sampleData;
+  // let templateVars = sampleData;
+
+  let templateVars = {
+    events: myEvents,
+    sortedBooks: sortedBooks
+    // sortedBooks: sampleData.sortedBooks
+  }
   
   res.render('table', templateVars);
+
   // res.send(JSON.stringify(books.data));
   // res.send(JSON.stringify(templateVars));
 });
