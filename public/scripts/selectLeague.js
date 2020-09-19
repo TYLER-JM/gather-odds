5fe0003ffce2de77f247d2b1ea0c5b024490f0e4
Tyler Martin <tyler.martin@electric-playground.com>
2020-09-19 16:07:54 -0300

    edits function generating the url to accomodate selecting type of odds

--- public/scripts/selectLeague.js
+++ public/scripts/selectLeague.js
@@ -1,8 +1,11 @@
-const selectLeague = (league) => {
+const selectLeague = () => {
+  let league = document.getElementById('league').value;
+  let line = document.getElementById('line').value
   let anchor = document.getElementById('getOddsLink');
   // console.log('select tag: ', elem);
-  anchor.setAttribute('href', `/odds/${league}`)
+  anchor.setAttribute('href', `/odds/${league}/${line}`)
 };
 
 //set the url when the page loads
-selectLeague(document.getElementById('league').value);
+// selectLeague(document.getElementById('league').value);
+selectLeague();
